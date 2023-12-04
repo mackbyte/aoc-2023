edc5a1787bde2ecc52bc24ae171e37382579d038
Patrick Mackinder <paddymackinder@gmail.com>
2023-12-04 10:18:20 -0700

    Implement auto part loader Parts can be selected to run by user input

--- aoc-2023/src/Program.cs
+++ aoc-2023/src/Program.cs
@@ -1,20 +1,26 @@
 ﻿using System;
+using System.Collections.Generic;
 using aoc_2023.common.part;
 
 namespace aoc_2023 {
     internal static class Program {
         public static void Main(string[] args) {
-            if (!args.Length.Equals(2)) {
-                Console.WriteLine("Please specify day and part numbers");
-                Environment.Exit(1);
-            }
+            PartDirectory.LoadParts();
 
-            int dayNum = int.Parse(args[0]);
-            int partNum = int.Parse(args[1]);
+            List<int> dayList = PartDirectory.GetDayNums();
+            Console.WriteLine("Please choose from the available days:");
+            dayList.ForEach(dayNum => Console.WriteLine($"[{dayNum}]: Day {dayNum}"));
+            int dayChoice = int.Parse(Console.ReadLine());
 
-            Part part = PartFactory.Create(dayNum, partNum);
+            List<int> partList = PartDirectory.GetPartNums(dayChoice);
+            Console.WriteLine("\nPlease choose from the available parts:");
+            partList.ForEach(partNum => Console.WriteLine($"[{partNum}]: Part {partNum}"));
+            int partChoice = int.Parse(Console.ReadLine());
+            
+            Console.WriteLine($"\nRunning Day {dayChoice} Part {partChoice}");
+            Part part = PartFactory.Create(dayChoice, partChoice);
             string result = part.Run();
-            Console.WriteLine(result);
+            Console.WriteLine($"Result: {result}");
         }
     }
 }
---
+++ aoc-2023/src/common/part/PartDirectory.cs
@@ -0,0 +1,59 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+
+namespace aoc_2023.common.part {
+    public static class PartDirectory {
+        private static readonly Dictionary<int, Day> Directory = new Dictionary<int, Day>();
+
+        private static void AddPart(string partPath) {
+            string[] dayAndPart = partPath.Split('.');
+            int dayNum = int.Parse(dayAndPart[0].Remove(0, 3)); // Remove day prefix
+            int partNum = int.Parse(dayAndPart[1].Remove(0, 4)); // Remove part prefix
+
+            Day day;
+            if (Directory.TryGetValue(dayNum, out day)) {
+                
+            } else {
+                day = new Day(dayNum);
+                day.SetPart(partNum);
+                Directory[dayNum] = day;
+            }
+        }
+        
+        public static void LoadParts() {
+            Assembly.GetExecutingAssembly()
+                .GetTypes()
+                .Where(t => t.IsClass && !t.Namespace.EndsWith("part") && typeof(Part).IsAssignableFrom(t))
+                .Select(t => t.FullName.Remove(0, 9)) // remove aoc_2023. prefix of the namespace
+                .ToList()
+                .ForEach(AddPart);
+        }
+
+        public static List<int> GetDayNums() {
+            return Directory.Keys.OrderBy(i => i).ToList();
+        }
+
+        public static List<int> GetPartNums(int dayNum) {
+            return Directory[dayNum].GetPartList();
+        }
+    }
+
+    public class Day {
+        public int Num { get; }
+        private readonly bool[] parts = { false, false };
+
+        public Day(int num) {
+            Num = num;
+        }
+
+        public void SetPart(int num) {
+            parts[num] = true;
+        }
+
+        public List<int> GetPartList() {
+            return parts.Where(isPart => isPart).Select((_, index) => index+1).ToList();
+        }
+    }
+}
